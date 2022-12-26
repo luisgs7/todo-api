b681a6c2f0b218959433f4624076c9c8d6c08cd1
luisgs7 <luisgsilva270@gmail.com>
2022-12-26 10:20:02 -0300

    config pylint

--- todolist/settings.py
+++ todolist/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from decouple import config
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
