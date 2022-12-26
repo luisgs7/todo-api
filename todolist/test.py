5b9cf35cb20270bf38dc1ed76c338653192d7149
luisgs7 <luisgsilva270@gmail.com>
2022-12-26 10:23:27 -0300

    one test

---
+++ todolist/test.py
@@ -0,0 +1,10 @@
+"""Module Test"""
+from unittest import TestCase
+
+
+class testSum(TestCase):
+    """Class TestSum"""
+
+    def test_add(self):
+        """Test add"""
+        self.assertEqual(2 + 1, 3)
